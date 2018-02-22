fa4dff1f363d3537536798de3207389cfb23ee46
Shalashalska <shalashalska@gmail.com>
2018-02-21 16:26:29 -0800

    Initial work on improve solve

--- algobowl.py
+++ algobowl.py
@@ -20,3 +20,37 @@ def read_input(inputfile):
     #except Exception as e:
     return connectionmap
 read_input('input.txt')
+
+
+#Solves the problem by picking random sets, then improving them
+def improveSolve(connectionmap):
+    #Populates two random sets with all points
+    left = set()
+    right = set()
+    for i in range(1, int(len(connectionmap) / 2)):
+        left.add(i)
+    for i in range(int(len(connectionmap) / 2) + 1, len(connectionmap)):
+        right.add(i)
+        
+    hasImprovements = true
+    while hasImprovements:
+        worst = 10000000
+        worstnum = 0
+        for i in range(1, len(connectiomap)):
+            if i in left:
+                total = 0
+                for j in i:
+                    if j in right:
+                        total += j[1]
+                if(total < worst):
+                    worst = total
+                    worstnum = i
+        best = 0
+        bestnum = 0
+        for j in connectionmap:
+            if i in right:
+                total = 0
+                for j in i:
+                    total += 1
+    
+    return (left, right)
